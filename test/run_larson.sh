0bc9c0e9b8be4e84046f197821f2773d94bc5c16
Zhiting Zhu <zhitingz@cs.utexas.edu>
2023-07-22 21:57:10 -0500

    change path
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- test/run_larson.sh
+++ test/run_larson.sh
@@ -8,16 +8,21 @@ ARGS="ALLOC="
 ARGS=${ARGS}${ALLOC}
 echo $ARGS
 
-make clean
-make larson_test ${ARGS}
+# make clean
+# make larson_test ${ARGS} FLAGS='-DSHM_SIMULATING'
+# if [ $ALLOC="r" ]; then
+# 	mv larson_test ./ralloc_bench
+# fi
 rm -rf larson.csv
 echo "thread,ops,allocator" >> larson.csv
-for i in {1..3}
-do
-	for threads in 1 2 4 6 10 16 20 24 32 40 48 62 72 80 84 88
-	do
+for i in {1..3}; do
+	for threads in 1 2 4 6 10 16 20 24 32 40 48 62 72 80 84 88; do
 		rm -rf /mnt/pmem/*
-		./larson-single.sh $threads $ALLOC
+		rm -rf /mnt/cxl_mem/*
+		rm -rf /dev/shm/*
+		if [ $ALLOC="r" ]; then
+			BINARY=./ralloc_bench/larson_test ./larson-single.sh $threads $ALLOC
+		fi
 	done
 done
 # SEDARGS="2,\$s/$/"
