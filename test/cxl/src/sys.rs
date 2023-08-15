e6c02802e32758b455309cc4dbc71e81857887b0
Newton Ni <nwtnni@gmail.com>
2023-08-15 12:55:29 -0500

    Add manual bindings for static inline CXL functions
    We can't link to them because static inline functions aren't
    compiled into static libraries. Although should the ralloc library
    be responsible for calling these? It doesn't seem like the application
    should know about the underlying kernel interface.

--- test/cxl/src/sys.rs
+++ test/cxl/src/sys.rs
@@ -2,4 +2,22 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use std::fs::File;
+use std::io;
+use std::os::fd::AsRawFd as _;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+const IOCTL_MAGIC: u32 = 0xf;
+
+ioctl_sys::ioctl!(try none _cxl_init_meta with IOCTL_MAGIC, 9);
+
+pub fn cxl_init_meta(cxl: &File) -> io::Result<()> {
+    unsafe { _cxl_init_meta(cxl.as_raw_fd()) }
+}
+
+ioctl_sys::ioctl!(try none _cxl_recover_meta with IOCTL_MAGIC, 10);
+
+pub fn cxl_recover_meta(cxl: &File) -> io::Result<()> {
+    unsafe { _cxl_recover_meta(cxl.as_raw_fd()) }
+}
